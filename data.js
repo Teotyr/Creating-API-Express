c51a980acb2970465254df3de0af00fb522ea8a8
Teotyr <dogukantahaydin@gmail.com>
2022-01-10 00:52:17 +0300

    Creating-API-Express
    Creating Actors Page Using Express

---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        isim: "Dwayne Jhonson",
+        filmler: ["Red Notice", "Jumanji", "Jungle Cruise", "Baywatch","BlackMan","Journey2","Rampage","Hercules","Aquaman"],
+    },
+    {
+        id: 2,
+        isim: "Tom Cruise",
+        filmler: ["Mission İmpossible","Top Gun", "Top Gun 2","Mummy","Eyes Wide Shut","War Of The Worlds","Last Samurai"],
+    },
+    {
+        id: 3,
+        isim: "Benedict Cumberbatch",
+        filmler: ["Doctor Strange","The Power Of Dog","Enigma","The Courier","The Electrical Life Of Louis Wain","Sherlock Holmes"],
+    },
+
+];
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const data = require('./data.js');
+const server =express();
+
+server.get('/',(req, res)=>{
+    res.send("Express Kullanarak Aktörler Sayfası Oluşturuyoruz")
+});
+
+server.get("/aktorler",(req, res)=>{
+res.status(200).json(data);
+});
+
+server.get("/aktorler/:id",(req, res)=>{
+
+    const { id } = req.params;
+    const aktor = data.find(aktor => aktor.id ===parseInt(id));
+    if(aktor){
+        res.status(200).json(aktor); 
+    }else {
+        res.status(404).send("Aradığınız Aktör Bulunamadı...");
+    }
+
+
+});
+
+server.listen(5000,()=>{
+    console.log("http//localhost:5000 adresinden gelen istekler dinleniyor...")
+});
